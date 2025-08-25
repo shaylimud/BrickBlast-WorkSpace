340707ecc34a3cf7c059bb7f4ff49f4bb9a25e50
shaylimud <118199047+shaylimud@users.noreply.github.com>
2025-08-25 15:21:02 +0300

    Guard against missing end currency listeners

--- Scripts/MyCode/Services/ResourceService.cs
+++ Scripts/MyCode/Services/ResourceService.cs
@@ -167,44 +167,52 @@ namespace Ray.Services
             EventService.Resource.OnMenuResourceChanged.Invoke(this);
         }
 
-        public void RewardBooster(BoosterType type)
+        public async void RewardBooster(BoosterType type)
         {
+            var saveData = Database.UserData.Copy();
+
             switch (type)
             {
                 case BoosterType.ClearRow:
-                    Database.UserData.Stats.Power_1 = Mathf.Clamp(Database.UserData.Stats.Power_1 + 1, 0, 99);
+                    saveData.Stats.Power_1 = Mathf.Clamp(saveData.Stats.Power_1 + 1, 0, 99);
                     break;
                 case BoosterType.ClearColumn:
-                    Database.UserData.Stats.Power_2 = Mathf.Clamp(Database.UserData.Stats.Power_2 + 1, 0, 99);
+                    saveData.Stats.Power_2 = Mathf.Clamp(saveData.Stats.Power_2 + 1, 0, 99);
                     break;
                 case BoosterType.ClearSquare:
-                    Database.UserData.Stats.Power_3 = Mathf.Clamp(Database.UserData.Stats.Power_3 + 1, 0, 99);
+                    saveData.Stats.Power_3 = Mathf.Clamp(saveData.Stats.Power_3 + 1, 0, 99);
                     break;
                 case BoosterType.ChangeShape:
-                    Database.UserData.Stats.Power_4 = Mathf.Clamp(Database.UserData.Stats.Power_4 + 1, 0, 99);
+                    saveData.Stats.Power_4 = Mathf.Clamp(saveData.Stats.Power_4 + 1, 0, 99);
                     break;
             }
+
+            await Database.Instance.Save(saveData);
 
             EventService.Resource.OnMenuResourceChanged.Invoke(this);
         }
 
-        public void ConsumeBooster(BoosterType type)
+        public async void ConsumeBooster(BoosterType type)
         {
+            var saveData = Database.UserData.Copy();
+
             switch (type)
             {
                 case BoosterType.ClearRow:
-                    Database.UserData.Stats.Power_1 = Mathf.Clamp(Database.UserData.Stats.Power_1 - 1, 0, 99);
+                    saveData.Stats.Power_1 = Mathf.Clamp(saveData.Stats.Power_1 - 1, 0, 99);
                     break;
                 case BoosterType.ClearColumn:
-                    Database.UserData.Stats.Power_2 = Mathf.Clamp(Database.UserData.Stats.Power_2 - 1, 0, 99);
+                    saveData.Stats.Power_2 = Mathf.Clamp(saveData.Stats.Power_2 - 1, 0, 99);
                     break;
                 case BoosterType.ClearSquare:
-                    Database.UserData.Stats.Power_3 = Mathf.Clamp(Database.UserData.Stats.Power_3 - 1, 0, 99);
+                    saveData.Stats.Power_3 = Mathf.Clamp(saveData.Stats.Power_3 - 1, 0, 99);
                     break;
                 case BoosterType.ChangeShape:
-                    Database.UserData.Stats.Power_4 = Mathf.Clamp(Database.UserData.Stats.Power_4 - 1, 0, 99);
+                    saveData.Stats.Power_4 = Mathf.Clamp(saveData.Stats.Power_4 - 1, 0, 99);
                     break;
             }
+
+            await Database.Instance.Save(saveData);
 
             EventService.Resource.OnMenuResourceChanged.Invoke(this);
         }
@@ -266,10 +274,14 @@ namespace Ray.Services
             int total = LevelCurrency.Value + LevelScore.Value;
 
             LevelCurrency.Value = total;
-            await Database.UserData.AddScoreAsCurrency(total);
+
+            if (Database.UserData != null)
+            {
+                await Database.UserData.AddScoreAsCurrency(total);
+            }
             LevelScore.Value = 0;
 
-            EventService.Resource.OnEndCurrencyChanged(this);
+            EventService.Resource.OnEndCurrencyChanged?.Invoke(this);
         }
 
         private async void RewardEndTriple(Component c)
@@ -284,7 +296,7 @@ namespace Ray.Services
 
             await Database.Instance.Save(saveData);
 
-            EventService.Resource.OnEndCurrencyChanged(this);
+            EventService.Resource.OnEndCurrencyChanged?.Invoke(this);
         }
         private async void RewardBrightData(Component c)
         {
