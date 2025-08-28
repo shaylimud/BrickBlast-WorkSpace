973a45a824018ace9ea5c8086fc21b8edf833279
shaylimud <shaylimud@gmail.com>
2025-08-28 11:52:26 +0300

    Changes

--- Scripts/MyCode/Shop.cs
+++ Scripts/MyCode/Shop.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    
-    
+
+    [Header("Shop-Items")] [SerializeField]
+    public GameObject bundlePrefab;
+
+    public GameObject itemPrefab;
+    public GameObject SpecialPrefab;
+    public void RefreshShopUI()
+    {
+        
+    }
 }
